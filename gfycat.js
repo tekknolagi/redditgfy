24eb2cc906ca360a93756686b1f2a2fc057d49fb
Max Bernstein <tekknolagi@gmail.com>
2014-01-01 18:22:03 -0800

    works with neverEndingLoad

--- gfycat.js
+++ gfycat.js
@@ -1,18 +1,31 @@
-$(function() {
-    String.prototype.endsWith = function(suffix) {
-        return this.indexOf(suffix, this.length - suffix.length) !== -1;
-    };
+loadGfycat = function loadGfycatF () {
+    console.log("loadGfycat called");
 
-    console.log("hi! gfycat extension loaded.");
-
-    $(".title.imgScanned").each(function() {
+    $(".title a.imgScanned").each(function() {
 	var href = $(this).attr("href");
 	
-	if (href.endsWith('gif')) {
+	if (href.endsWith('gif') && !href.startsWith('http://gfycat.com')) {
 	    $(this).attr("href",
 			 "http://gfycat.com/fetch/"+href);
 	}
-    });
+    });    
+};
 
-    console.log("bye! gfycat extension finished.");
+waitLoadGfycat = function waitLoadGfycatF () {
+    setTimeout(loadGfycat, 500);
+}
+
+$(function () {
+    String.prototype.endsWith = function (suffix) {
+	return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+
+    String.prototype.startsWith = function (prefix) {
+	return this.indexOf(prefix) == 0;
+    };
+
+    loadGfycat();
+    $(window).bind("neverEndingLoad", waitLoadGfycat);
 });
+
+
